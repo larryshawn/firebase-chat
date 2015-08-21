0a475e7ac128570cbedc8781254e801bd1ad2576
Larry Babcock <larryshawn@gmail.com>
2015-08-21 14:56:07 -0700

    pushing to view on iphone

--- chat.js
+++ chat.js
@@ -5,31 +5,31 @@
 // create firebase reference 
 var myDataRef = new Firebase('https://vivid-inferno-6632.firebaseio.com/');
       $('#messageInput').keypress(function (e) {
-        button = $('#go');
-        console.log(button);
-        if (e.keyCode == 13 || $('button').click()) { // if enter key is pressed
-          var name = $('#nameInput').val();
-          var text = $('#messageInput').val();
-          myDataRef.push({name: name, text: text}); // immediately push the user inputs to the database
+        if (e.keyCode == 13) { // if enter key is pressed
+          var name = $('#nameInput').val(); // get the value from nameInput field
+          var text = $('#messageInput').val(); // get the value from messageInput field
+          myDataRef.push({name: name, text: text}); // immediately push those user inputs to the database
           $('#messageInput').val(''); // clear the messageInput box for the next message
         }
       });
+      $('#go').click(function() {
+          var name = $('#nameInput').val(); // get the value from nameInput field
+          var text = $('#messageInput').val(); // get the value from messageInput field
+          myDataRef.push({name: name, text: text}); // immediately push those user inputs to the database
+          $('#messageInput').val(''); // clear the messageInput box for the next message
+        });
       myDataRef.on('child_added', function(snapshot) { // when chat arrives
         var message = snapshot.val(); // store the value of items added
         displayChatMessage(message.name, message.text); // pass name and text to displayChatMessage()
-        console.log(snapshot.val());
-        // scroll to the last message
-      $('#messages')[0].scrollTop = $('#messages')[0].scrollHeight;
       });
 
       function displayChatMessage(name, text) {
         $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messages'));
         $('#messages')[0].scrollTop = $('#messages')[0].scrollHeight;
       };
-      
 // end Firebase
 
-$('#go').click(function() {
-        alert('Submit 2 clicked');
-    }
-);
+// $('#go').click(function() {
+//         alert('Submit 2 clicked');
+//     }
+// );
